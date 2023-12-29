3e7f2a684077717a78e980d0c0eb8549566dd2f1
Eneilac <eduarneila@gmail.com>
2023-12-29 11:00:48 +0100

    fix comments

--- mouse-follower/src/components/FollowMouse.jsx
+++ mouse-follower/src/components/FollowMouse.jsx
@@ -2,8 +2,8 @@ import { useEffect, useState } from 'react'
 import './followMouse.css'
 
 export const FollowMouse = () => {
-  const [enabled, setEnabled] = useState(true)
-  const [position, setPosition] = useState({ x: 0, y: 0 })
+  const [enabled, setEnabled] = useState(true) //estado para el botón
+  const [position, setPosition] = useState({ x: 0, y: 0 }) //cordenadas del cursor
 
   useEffect(() => {
     const handleMove = (event) => {
@@ -15,14 +15,17 @@ export const FollowMouse = () => {
       window.addEventListener('pointermove', handleMove)
     }
 
+    //Limpia el efecto siempre que se desmonte el componente o cambie el estado de la dependencia
     return () => {
       window.removeEventListener('pointermove', handleMove)
     }
   }, [enabled])
 
+  //cuando en el stado esta true quitamos el cursor con una clase css
   useEffect(() => {
     document.body.classList.toggle('no-cursor', enabled)
 
+    //Limpiamos el efecto
     return () => {
       document.body.classList.remove('no-cursor')
     }
@@ -31,11 +34,11 @@ export const FollowMouse = () => {
   return (
     <main>
       <div style={{
-        transform: `translate(${position.x}px, ${position.y}px)`
+        transform: `translate(${position.x}px, ${position.y}px)` //solo damos este estilo en linea ya que necesitamos las cordenadas
       }}
         className='bolita' />
       <button onClick={() => setEnabled(!enabled)}>
-        {enabled ? 'Desactivar' : 'Activar'} seguir puntero
+        {enabled ? 'Desactivar' : 'Activar'} seguir raton
       </button>
     </main>
   )
